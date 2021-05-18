20efabf2abf7a0f0e08d24df0bdf9ac4c9e412e7
superhetja <42982342+superhetja@users.noreply.github.com>
2021-05-18 09:03:26 +0000

    first commit

---
+++ arborg_subtheme/js/global.js
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.arborg_subtheme = {
+    attach: function (context, settings) {
+      //var btnClick = document.getElementsByClassName('btn-card');
+      //console.log(btnClick);
+      //$(".btn-card").click(e => {
+        //e.preventDefault();
+        //console.log(e.target.href);
+      //});
+
+
+    }
+  };
+
+})(jQuery, Drupal);
